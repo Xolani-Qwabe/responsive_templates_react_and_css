775cac296fcf39f4edea1fe8f92191a48ca1102d
xolani <thabisoxolaniqwabe@gmail.com>
2024-02-07 01:28:21 +0200

    frontend home vs away game stats prediction

---
+++ app/src/App.jsx
@@ -0,0 +1,37 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className="wrapper">
+        <nav className="header">
+          <h1>NavBAr</h1>
+        </nav>
+        <article className="main">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Possimus, nesciunt. Numquam, maiores debitis quibusdam 
+            deleniti at animi expedita. Modi iusto dolorum dolores hic? 
+            Ea vero reprehenderit atque placeat ipsam velit.
+          </p>
+        </article>
+        <aside className="aside team2">
+          <h1 >Team 2</h1>
+        </aside>
+        <aside className="aside totals">
+          <h1 >Totals</h1>
+        </aside>
+        <aside className="aside team1">
+          <h1 >Team 1</h1>
+        </aside>
+        <footer className="footer">
+          <p>Lorem ipsum dolor sit amet, consectetur adipacol.</p>
+        </footer>
+      </div>
+
+    </>
+  )
+}
+
+export default App
